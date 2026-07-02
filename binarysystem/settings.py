5b68db26ca49e3d74f4d9f2486452dacfb083d9d
Mallipudisri99 <srivamsi787@gmail.com>
2026-07-02 12:04:07 +0530

    Move SMTP credentials to environment variables

--- binarysystem/settings.py
+++ binarysystem/settings.py
@@ -366,16 +366,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # X_FRAME_OPTIONS = "DENY"
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 EMAIL_HOST = "smtp.gmail.com"
-
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "genesisits369@gmail.com"
-
-EMAIL_HOST_PASSWORD = "qmcl obio dfrw avbm"
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
